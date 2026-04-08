98090bce7a5eb1d200d483f4b89585d3308c358d
Kalash Bhadoriya <89040023+Jis-4@users.noreply.github.com>
2026-04-08 15:43:29 +0530

    feat: add 80 COCO class labels and colors

---
+++ lib/classes.ts
@@ -0,0 +1,29 @@
+export const COCO_CLASSES: string[] = [
+  'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck',
+  'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench',
+  'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra',
+  'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee',
+  'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove',
+  'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup',
+  'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange',
+  'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch',
+  'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse',
+  'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink',
+  'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier',
+  'toothbrush'
+];
+
+export const CLASS_COLORS: string[] = [
+  '#FF3366','#FF6633','#FFCC33','#33FF66','#33CCFF','#CC33FF','#FF33CC',
+  '#33FFCC','#FF9933','#3366FF','#FF3399','#99FF33','#33FF99','#9933FF',
+  '#FF9966','#66FF33','#3399FF','#FF6699','#33FF66','#6633FF','#FF3366',
+  '#33FFCC','#FF9933','#CC33FF','#33FF99','#FF6633','#3366FF','#FFCC33',
+  '#FF33CC','#33CCFF','#FF3399','#99FF33','#9933FF','#FF9966','#66FF33',
+  '#3399FF','#FF6699','#33FF66','#6633FF','#FF3366','#33FFCC','#FF9933',
+  '#CC33FF','#33FF99','#FF6633','#3366FF','#FFCC33','#FF33CC','#33CCFF',
+  '#FF3399','#99FF33','#9933FF','#FF9966','#66FF33','#3399FF','#FF6699',
+  '#33FF66','#6633FF','#FF3366','#33FFCC','#FF9933','#CC33FF','#33FF99',
+  '#FF6633','#3366FF','#FFCC33','#FF33CC','#33CCFF','#FF3399','#99FF33',
+  '#9933FF','#FF9966','#66FF33','#3399FF','#FF6699','#33FF66','#6633FF',
+  '#FF3366','#33FFCC','#FF9933','#CC33FF'
+];
